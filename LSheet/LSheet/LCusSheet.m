b8d56b7f64ea409d32e0b4cdba16f07ba30e685a
ponted <ponted@ponteddeMacBook-Pro.local>
2018-02-28 15:50:32 +0800

    change code1

--- LSheet/LSheet/LCusSheet.m
+++ LSheet/LSheet/LCusSheet.m
@@ -154,7 +154,7 @@
 
 - (void)hide
 {
-    __unsafe_unretained LCusSheet *weakSelf = self;
+     LCusSheet * __weak weakSelf = self;
     CGRect frame = _sheetTableView.frame;
     [UIView animateWithDuration:0.25 animations:^{
         weakSelf.backgroundColor = [UIColor colorWithWhite:0.3 alpha:0];
